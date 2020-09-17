530f337e5452b31e128637ca04060a5d65e39e13
Alessandro <alessandro.boscato@gmail.com>
2020-09-17 14:42:36 +0200

    Inizio progetto

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+//   Facciamo una chiamata ajax all'api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L'api ci restituirà decina di dischi musicali che dovremo stampare a schermo con Handlebars.
+// Concentratevi sulla parte JS per la grafica potrete utilizzare il layout che troverete al seguente link
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout/downloads/
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
+
+
+});
