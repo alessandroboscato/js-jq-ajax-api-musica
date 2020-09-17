1584f7c04e2db001a8a9d34b06a04a83b694a4a0
Alessandro <alessandro.boscato@gmail.com>
2020-09-17 15:41:20 +0200

    fine esercizio

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
+  var source = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
+
 //   Facciamo una chiamata ajax all'api di boolean.
 $.ajax({
 
   "url": "https://flynn.boolean.careers/exercises/api/array/music",
   "method": "GET",
   "success": function (data, stato) {
-    console.log(data.response);
-    // L'api ci restituirà decina di dischi musicali che dovremo stampare a schermo con Handlebars.
-
+    var listaDischi = data.response;
+    // L'api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Handlebars.
+    for (var i = 0; i < listaDischi.length; i++) {
+      var html = template(listaDischi[i]);
+      $("#cd-player").append(html);
+    }
   },
   "error": function (richiesta, stato, errori) {
   alert("E' avvenuto un errore. " + errore);
   }
   });
 
-//
-// Concentratevi sulla parte JS per la grafica potrete utilizzare il layout che troverete al seguente link
-// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout/downloads/
+
 // Bonus:
 // Creare una select con i seguenti generi: pop, rock, metal e jazz.
 // In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
